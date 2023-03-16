479e7962dd040a15a2ac88d79c40ab0cd6e620e2
mankaa <mankaache10@gmail.com>
2023-03-16 07:08:04 +0100

    succesfully added the delete blog functionality

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,19 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./UseFetch";
 
 const BlogDetails = () => {
     const { id } = useParams()
     const {data:blog, error, isPending} = useFetch('http://localhost:8000/blogs/' + id)
-
+    const history = useHistory()
+    
+    
+    function handleDelete(){
+        fetch('http://localhost:8000/blogs/' + blog.id,{
+            method: 'DELETE'
+        }).then(()=>{
+            history.push('/')
+        })
+    }
 
     return ( 
         <div className="blog-details">
@@ -15,6 +24,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by  {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleDelete}>delete</button>
                 </article>
             )}
         </div>
