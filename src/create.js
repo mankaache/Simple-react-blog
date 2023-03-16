c41ef4fbc3a03ef34944ebdb8429f569e93aa989
mankaa <mankaache10@gmail.com>
2023-03-16 06:46:42 +0100

    created the object that will be sent to json server

--- src/create.js
+++ src/create.js
@@ -1,9 +1,56 @@
+import { useState } from "react";
+
 const Create = () => {
-    return ( 
-        <div className="create">
-            <h2>Add a New Blog</h2>
-        </div>
-     );
-}
- 
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    // the object we will store the info that hte user enters
+    const blog = {
+        title,
+        body,
+        author
+    }
+    
+  }
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog Title:</label>
+        {/* adding a blog title */}
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+
+        {/* adding a blog paragraph */}
+        <textarea 
+        value={body}
+        onChange={(e)=>setBody(e.target.value)}
+        required></textarea>
+        <label>Blog Author</label>
+
+        {/* selecting the blog author */}
+        <select
+        value={author}
+        onChange={(e)=>setAuthor(e.target.value)}
+        >
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+
+        {/* adding a new blog */}
+        <button>Add blog</button>
+        <p>{body}</p>
+      </form>
+    </div>
+  );
+};
+
 export default Create;
